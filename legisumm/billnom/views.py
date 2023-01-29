329a0d9a7811be4868290e66af36ab5650756514
andrew amakye ansah <eikeaaa15@gmail.com>
2023-01-29 13:16:18 -0600

    Base Website

--- legisumm/billnom/views.py
+++ legisumm/billnom/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import path
+from django.views.generic import RedirectView
+from . import views
+from django.conf.urls.i18n import i18n_patterns 
 
 # Create your views here.
+def indexbase(request):
+    return render(request, 'billnom/index.html',{})
+
+def retsumm(request):
+    return render(request, 'billnom/billTex.html', {})
